561471d30638d06eff346c99c08640dc1bfe892c
GitHub Action <action@github.com>
2023-08-25 10:10:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/young4dream/bmsgitcommitmessage/MyBundle.kt
+++ src/main/kotlin/com/github/young4dream/bmsgitcommitmessage/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.young4dream.bmsgitcommitmessage
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/young4dream/bmsgitcommitmessage/services/MyProjectService.kt
+++ src/main/kotlin/com/github/young4dream/bmsgitcommitmessage/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.young4dream.bmsgitcommitmessage.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.young4dream.bmsgitcommitmessage.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
